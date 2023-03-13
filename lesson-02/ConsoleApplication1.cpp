40178bc96c1df8a87aa29506ef3a4d41a1398bda
KacperS123 <126490043+KacperS123@users.noreply.github.com>
2023-03-13 16:08:13 +0100

    Add files via upload

---
+++ lesson-02/ConsoleApplication1.cpp
@@ -0,0 +1,39 @@
+﻿// ConsoleApplication1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+class computer{
+public:
+	string hardware;
+	string peripherals;
+};
+class personaldata
+{
+public:
+	int age;
+	string name;
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+	computer test;
+	test.hardware ="CPU";
+	test.peripherals = "Mouse";
+	personaldata test1;
+	test1.age = 20;
+	test1.name = "Kacper";
+	cout << test1.name << endl;
+
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
