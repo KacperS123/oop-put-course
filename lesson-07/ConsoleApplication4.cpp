8063e072a5b2907516188a43976049c3d7f41df3
KacperS123 <126490043+KacperS123@users.noreply.github.com>
2023-04-24 16:11:49 +0200

    Add files via upload

---
+++ lesson-07/ConsoleApplication4.cpp
@@ -0,0 +1,40 @@
+﻿// ConsoleApplication4.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <sstream>
+#include <memory>
+using namespace std;
+class Sequence
+{
+public:
+	virtual int lenght() = 0;
+	class Fake;
+};
+class Sequence::Fake : public Sequence {
+	int lenght() { return 100; };
+};
+class Gene
+{
+private:
+	string name_;
+	shared_ptr<Sequence> sequence_;
+public:
+	Gene() {
+		Gene(string name, shared_ptr<Sequence> sequence)
+			:name_(name), sequence_(sequence) {};
+		string JSON() {
+			stringstream ss
+				ss << "{";
+			ss << " \"name\"; \"" << name_ << "\",";
+			ss << " \"lenght\"; " << sequence_ ->Lenght();
+			return ss.str();
+		}
+	}
+};
+int main()
+{
+	shared_ptr<Sequence> fakeSequence = make_shared<Sequence::Fake>();
+	Gene gene(name: "BALFS", sequence: fakeSequence);
+	return 0;
+}
