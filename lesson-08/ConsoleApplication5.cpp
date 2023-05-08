33eb1f2914489ef8e02f1b131bb544c69e525c6c
KacperS123 <126490043+KacperS123@users.noreply.github.com>
2023-05-08 16:15:43 +0200

    Add files via upload

---
+++ lesson-08/ConsoleApplication5.cpp
@@ -0,0 +1,40 @@
+﻿// ConsoleApplication5.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <cmath>
+
+class Logarithm {
+public:
+	Logarithm(double base, double argument) {
+		base_ = base;
+		argument_ = argument;
+	}
+
+	double Calculate() const {
+		if (base_ == 0 || base_ == 1) throw std::invalid_argument("Podstawa musi być większa od 0 i różna od 1");
+
+		return std::log(argument_) / std::log(base_);
+	}
+private:
+	double base_;
+	double argument_;
+};
+int main() {
+	Logarithm test(4, 2);
+	try {
+		test.Calculate();
+	} catch (std::invalid_argument & e) {
+	}
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
